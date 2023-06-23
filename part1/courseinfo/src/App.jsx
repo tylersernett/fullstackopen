28dffc7c35e3789e8830914a7c1e1c355a4ec29b
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-06-23 18:34:40 -0400

    Add Part component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,25 +1,22 @@
-const Header = ({course}) => {
-  // const { course } = props;
+const Header = ({ course }) => {
   return <h1>{course}</h1>;
 };
 
-const Content = ({ part1, part2, part3, exercises1, exercises2, exercises3 } ) => {
+const Content = ({ part1, part2, part3, exercises1, exercises2, exercises3 }) => {
   return (
     <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
     </>
   );
 };
 
-const Total = ( { exercises1, exercises2, exercises3 }) => {
+const Part = ({ part, exercises }) => {
+  return <p> {part} {exercises} </p>
+}
+
+const Total = ({ exercises1, exercises2, exercises3 }) => {
   return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
 };
 
