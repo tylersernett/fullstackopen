f53df43f7f5ce5f52fbb56cbcbaf19b2e32148f3
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-06-23 19:28:52 -0400

    Convert vars into single object

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,5 +1,5 @@
 const Header = ({ course }) => {
-  return <h1>{course}</h1>;
+  return <h1>{course.name}</h1>;
 };
 
 const Content = ({ parts }) => {
@@ -21,21 +21,32 @@ const Total = ({ parts }) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 },
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
       <Content
-        parts={parts}
+        parts={course.parts}
       />
       <Total
-        parts={parts}
+        parts={course.parts}
       />
     </div>
   );
