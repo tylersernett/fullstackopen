75ceed0a89e0e38b1b002444786779c1d445617a
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-06-24 15:14:58 -0400

    Add basic button functionality

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// const Button = ({ label, function }) => {
+//   return (
+//     <button onClick={() => function((prev) => prev + 1) }>{label}</button>
+//   )
+// }
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>give feedback</h2>
+      <button onClick={() => setGood((prev)=> prev+1)}>good</button>
+      <button onClick={() => setNeutral((prev)=> prev+1)}>neutral</button>
+      <button onClick={() => setBad((prev)=> prev+1)}>bad</button>
+
+      <h2>statistics</h2>
+      good: {good}
+      <br/>
+      neutral: {neutral}
+      <br/>
+      bad: {bad}
+    </div>
   )
 }
 
