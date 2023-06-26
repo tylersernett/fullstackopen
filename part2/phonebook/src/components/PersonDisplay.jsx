c6935c7b754edbab9275cb38e594cf80b118f590
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-06-26 00:16:04 -0400

    Extract PersonDetails component

--- part2/phonebook/src/components/PersonDisplay.jsx
+++ part2/phonebook/src/components/PersonDisplay.jsx
@@ -1,9 +1,15 @@
+const PersonDetails = ({ person }) => {
+    return (
+        <li>{person.name}: {person.number}</li>
+    )
+}
+
 const PersonDisplay = ({ namesToShow }) => {
     return (
         <>
             <h3>Numbers</h3>
             <ul>
-                {namesToShow.map(person => <li key={person.name}>{person.name}: {person.number}</li>)}
+                {namesToShow.map(person => <PersonDetails person={person} key={person.name} />)}
             </ul>
         </>
     )
