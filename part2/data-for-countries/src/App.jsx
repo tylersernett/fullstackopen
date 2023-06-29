7e25794f0b226c0f54fd0424c08f6e405b47a853
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-06-29 18:47:53 -0400

    Add country filter

--- part2/data-for-countries/src/App.jsx
+++ part2/data-for-countries/src/App.jsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
+const Search = ({ searchString, setSearchString }) => {
+  return (
+    <>
+      Search countries: <input value={searchString} onChange={(e) => setSearchString(e.target.value)}></input>
+    </>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [countries, setCountries] = useState(null);
+  const [searchString, setSearchString] = useState("");
+
+  const filteredCountries = countries.filter(country =>
+    country.name.common.toLowerCase().includes(searchString.toLowerCase()));
+
+  useEffect(() => {
+    axios.get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then(response => setCountries(response.data))
+      .catch(error => console.log(error))
+
+    return () => {
+    }
+  }, [])
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Search searchString={searchString} setSearchString={setSearchString} />
+
+      {searchString !== "" &&
+        filteredCountries.length === 1 ? (
+        <div>match</div>
+      )
+        :
+        filteredCountries.length <= 10 ? (
+          filteredCountries.map(country =>
+            <div key={country.name.common}>{country.name.common}</div>
+          )
+        ) : (
+          <div>Too many matches, specify another filter</div>
+        )
+      }
     </>
   )
 }
