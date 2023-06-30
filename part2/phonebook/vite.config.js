43a8a542e53eb78236ddf0beea74b072f55f79e4
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-06-30 14:29:52 -0400

    Add proxy for local use

--- part2/phonebook/vite.config.js
+++ part2/phonebook/vite.config.js
@@ -1,7 +1,17 @@
+// import { createProxyMiddleware } from 'http-proxy-middleware';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+    }
+  }
 })
