97dc34145273f241f17abfdbbf2e580431507871
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-02 22:29:07 -0400

    Extract logger

--- part4-blog-list/app.js
+++ part4-blog-list/app.js
@@ -10,7 +10,4 @@ app.use('/api/blogs', blogsRouter)
 app.use(cors())
 app.use(express.json())
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+module.exports = app
--- part4-blog-list/index.js
+++ part4-blog-list/index.js
@@ -1,1 +1,7 @@
 const app = require('./app') 
+const logger = require('./utils/logger')
+
+const PORT = 3003
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`)
+})
