f3fea17e48f445015fcc1be4983494e17751a4fa
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-02 22:15:49 -0400

    Extract blog model

--- part4-blog-list/app.js
+++ part4-blog-list/app.js
@@ -2,19 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+const Blog = require('./models/blog')
 
 app.use(cors())
 app.use(express.json())
