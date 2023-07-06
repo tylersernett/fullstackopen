e6d857e102ad7560ccff7c3d41b6d38be4b76d0e
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-05 22:43:59 -0400

    Move routes below JSON body parser

--- part4-blog-list/app.js
+++ part4-blog-list/app.js
@@ -3,11 +3,12 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const Blog = require('./models/blog')
-const blogsRouter = require('./controllers/blogs')
-app.use('/api/blogs', blogsRouter)
-
 
 app.use(cors())
 app.use(express.json())
+
+//define routes AFTER the body parser or request.body is undefined
+const blogsRouter = require('./controllers/blogs')
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
