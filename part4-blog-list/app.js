3cf3ae3c8ed4ed2ae1d6ec70ea4837794577d39f
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-05 23:30:19 -0400

    Install express-async-errors

--- part4-blog-list/app.js
+++ part4-blog-list/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+require('express-async-errors')
 const cors = require('cors')
 const Blog = require('./models/blog')
 
