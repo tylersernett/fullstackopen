50fa8319b4372f1dc389d271ced4dbfe320fcd77
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-08 18:09:56 -0400

    Move connect code, fix api/users url

--- part4-blog-list/app.js
+++ part4-blog-list/app.js
@@ -1,7 +1,13 @@
 require('dotenv').config()
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 require('express-async-errors')
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl)
+
 const cors = require('cors')
 const Blog = require('./models/blog')
 
@@ -13,6 +19,6 @@ const blogsRouter = require('./controllers/blogs')
 app.use('/api/blogs', blogsRouter)
 
 const usersRouter = require('./controllers/users')
-app.use('./api/users', usersRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
