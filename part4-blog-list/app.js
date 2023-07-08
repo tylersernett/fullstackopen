47e1e15bebbbbceca4e8b73a2cee5781f0ab43b1
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-08 19:07:55 -0400

    Add proper middleware handling

--- part4-blog-list/app.js
+++ part4-blog-list/app.js
@@ -5,6 +5,8 @@ const app = express()
 const mongoose = require('mongoose')
 require('express-async-errors')
 
+const middleware = require('./utils/middleware')
+
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
 
@@ -13,12 +15,15 @@ const Blog = require('./models/blog')
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 //define routes AFTER the body parser or request.body is undefined
 const blogsRouter = require('./controllers/blogs')
 app.use('/api/blogs', blogsRouter)
-
 const usersRouter = require('./controllers/users')
 app.use('/api/users', usersRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
