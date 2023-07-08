16db3a5120cb1f56bfa8ee8a6cdf7b38afeae848
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-08 18:39:52 -0400

    Add pre-hash password length check

--- part4-blog-list/controllers/users.js
+++ part4-blog-list/controllers/users.js
@@ -10,6 +10,10 @@ usersRouter.get('/', async (requests, response) => {
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
+  if (password.length < 3) {
+    return response.status(400).json({error: 'password too short'})
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
