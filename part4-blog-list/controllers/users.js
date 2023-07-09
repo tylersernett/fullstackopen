d474e5901b4d3ad5f7f0675076c99c420e0a7ea1
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-09 00:33:34 -0400

    Associate blogs to their active user

--- part4-blog-list/controllers/users.js
+++ part4-blog-list/controllers/users.js
@@ -3,7 +3,7 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (requests, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate('blogs', { url: 1, title: 1, author:1 })
   response.status(200).json(users);
 })
 
