acf2c7536c27c3c4613b796a67c443a16868f9d9
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-09 03:45:03 -0400

    Differentiate between 400 and 401 errors

--- part4-blog-list/utils/middleware.js
+++ part4-blog-list/utils/middleware.js
@@ -40,7 +40,11 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
   } else if (error.name === 'JsonWebTokenError') {
-    return response.status(400).json({ error: error.message })
+    if (error.message === 'jwt must be provided') {
+      return response.status(401).json({ error: error.message })
+    } else {
+      return response.status(400).json({ error: error.message })
+    }
   } else if (error.name === 'PWError') {
     return response.status(400).json({ error: error.message })
   } else if (error.name === 'TokenExpiredError') {
