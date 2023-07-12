023029f9d4cd5a017e2cfd94b63bf84f1fea7ee4
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-11 23:49:08 -0400

    Add show/hide details button

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,24 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const { title, author, url, likes, user } = blog;
+
+  return (
+    <div>
+      {title} - {author}<button onClick={() => setShowDetails(!showDetails)}>
+        {showDetails ? "hide" : "view"}
+      </button>
+      {showDetails &&
+        <>
+          <div>{url}</div>
+          <div>likes: {likes}</div>
+          <div>{user.name}</div>
+        </>
+      }
+    </div>
+  )
+}
 
 export default Blog
