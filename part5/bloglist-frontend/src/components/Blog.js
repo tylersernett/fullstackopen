fd33fd4a1a5dd377e2751fff0824d717658c69fd
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-11 23:54:11 -0400

    Add blog styles

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -2,11 +2,16 @@ import { useState } from "react"
 
 const Blog = ({ blog }) => {
   const [showDetails, setShowDetails] = useState(false);
-
   const { title, author, url, likes, user } = blog;
-
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
   return (
-    <div>
+    <div style={blogStyle}>
       {title} - {author}<button onClick={() => setShowDetails(!showDetails)}>
         {showDetails ? "hide" : "view"}
       </button>
