4e048f7aeb98ce493325fcb14c7f423a3c38a34c
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-12 00:35:37 -0400

    Sort blogs by # of likes

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -101,9 +101,10 @@ const App = () => {
           </Togglable>
 
           <h2>blogs</h2>
-          {blogs.map(blog =>
-            <Blog key={blog.id} blog={blog} handleLike={handleLike} />
-          )}
+          {blogs
+            .sort((a, b) => b.likes - a.likes) // Sort the blogs array based on likes in descending order
+            .map(blog => <Blog key={blog.id} blog={blog} handleLike={handleLike} />)
+          }
         </>
       }
     </div>
