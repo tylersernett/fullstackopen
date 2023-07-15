daa97df2658e975d2e6b2908eaa3c1fdbe1a1328
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-15 14:31:40 -0400

    Add login & blog create tests

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,7 +1,43 @@
-describe('Note app', function() {
-  it('front page can be opened', function() {
+describe('Note app', function () {
+  beforeEach(function () {
     cy.visit('http://localhost:3000')
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Blab Blabby',
+      username: 'newuser',
+      password: 'bbbbbb'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
+  })
+
+  it('front page can be opened', function () {
     cy.contains('Blogs')
-    // cy.contains('Note app, Department of Computer Science, University of Helsinki 2023')
+  })
+
+  it('login form can be opened', function () {
+    cy.get('#username').type('newuser')
+    cy.get('#password').type('bbbbbb')
+    cy.get('#login-button').click()
+
+    cy.contains('Blab Blabby logged in')
+  })
+
+  describe('when logged in', function () {
+    beforeEach(function () {
+      cy.get('#username').type('newuser')
+      cy.get('#password').type('bbbbbb')
+      cy.get('#login-button').click()
+    })
+
+    it('a new blog can be created', function () {
+      cy.contains('new blog').click()
+      cy.get('.blogform-title').type('test blog')
+      cy.get('.blogform-author').type('Testy Test')
+      cy.get('.blogform-url').type('test.com')
+      cy.contains('create').click()
+
+      cy.contains('test blog - Testy Test')
+
+    })
   })
 })
