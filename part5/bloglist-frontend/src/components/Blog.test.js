f23a6eeadc1b2852fedcf7b42a21246d1f55fb2c
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-15 16:54:37 -0400

    Correct typo

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -4,7 +4,7 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-describe('<Blog> />', () => {
+describe('<Blog/>', () => {
 
   let blog
   let mockLikeHandler
