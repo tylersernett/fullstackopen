c6f12aef118f90180c9205fee180cda61214aba1
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-15 14:49:05 -0400

    Add like button testing

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,4 +1,4 @@
-describe('Note app', function () {
+describe('Blog app', function () {
   beforeEach(function () {
     cy.visit('http://localhost:3000')
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
@@ -14,22 +14,35 @@ describe('Note app', function () {
     cy.contains('Blogs')
   })
 
-  it('login form can be opened', function () {
-    cy.get('#username').type('newuser')
-    cy.get('#password').type('bbbbbb')
-    cy.get('#login-button').click()
-
-    cy.contains('Blab Blabby logged in')
+  it('Login form is shown', function () {
+    cy.contains('Log in to application')
+    cy.contains('username')
+    cy.contains('password')
   })
 
-  describe('when logged in', function () {
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+
+      cy.get('#username').type('newuser')
+      cy.get('#password').type('bbbbbb')
+      cy.get('#login-button').click()
+
+      cy.contains('Blab Blabby logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      // ...
+    })
+  })
+
+  describe('When logged in', function () {
     beforeEach(function () {
       cy.get('#username').type('newuser')
       cy.get('#password').type('bbbbbb')
       cy.get('#login-button').click()
     })
 
-    it('a new blog can be created', function () {
+    it('A blog can be created', function () {
       cy.contains('new blog').click()
       cy.get('.blogform-title').type('test blog')
       cy.get('.blogform-author').type('Testy Test')
@@ -37,7 +50,34 @@ describe('Note app', function () {
       cy.contains('create').click()
 
       cy.contains('test blog - Testy Test')
+    })
+
+    describe('and a blog exists', function () {
+      beforeEach(function () {
+        cy.contains('new blog').click()
+        cy.get('.blogform-title').type('test blog')
+        cy.get('.blogform-author').type('Testy Test')
+        cy.get('.blogform-url').type('test.com')
+        cy.contains('create').click()
+      })
+
+      it('its likes can be increased', function () {
+        cy.contains('test blog - Testy Test')
+          .contains('view')
+          .click()
+
+        cy.contains('test blog - Testy Test')
+          .contains('likes: 0')
+
+        cy.contains('test blog - Testy Test')
+          .contains('like')
+          .click()
+
+        cy.contains('test blog - Testy Test')
+          .contains('likes: 1')
+      })
 
     })
   })
+
 })
