77ce2c59881775a658ded257643570d2fb5a1f9e
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-18 13:48:47 -0400

    Refactor to anecdoteReducer

--- part6/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ part6/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -19,7 +19,7 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
+const anecdoteReducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
@@ -64,4 +64,4 @@ export const increaseVoteOf = (id) => {
   }
 }  
 
-export default reducer
+export default anecdoteReducer
