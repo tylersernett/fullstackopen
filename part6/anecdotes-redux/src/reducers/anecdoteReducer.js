ee149c1253a46e286b33acfa61b258012c1b9b87
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-18 10:11:52 -0400

    Add action handling

--- part6/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ part6/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -23,7 +23,24 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch (action.type) {
+    case 'VOTE':
+      const id = action.payload.id
+      const anecdoteToChange = state.find(anec => anec.id === id)
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1
+      }
+      return state.map(anecdote =>
+        anecdote.id !== id ? anecdote : changedAnecdote
+      )
+
+    case 'NEW_ANECDOTE':
+      return [...state, action.payload]
+
+    default:
+      return state
+  }
 }
 
 export default reducer
