e516b7ece872d5d67d4882ebc79f7aaaf1af4fbe
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-18 12:41:34 -0400

    Extrat AnecdoteList to new file

--- part6/anecdotes-redux/src/App.js
+++ part6/anecdotes-redux/src/App.js
@@ -1,33 +1,12 @@
 //part 6 hours:  2
-import { useSelector, useDispatch } from 'react-redux'
-import { createAnecdote, increaseVoteOf } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    console.log('vote', id)
-    dispatch(increaseVoteOf(id))
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes
-        .sort((a, b) => b.votes - a.votes)
-        .map(anecdote =>
-          <div key={anecdote.id}>
-            <div>
-              {anecdote.content}
-            </div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => vote(anecdote.id)}>vote</button>
-            </div>
-          </div>
-        )}
+      <AnecdoteList/>
       <AnecdoteForm/>
     </div>
   )
