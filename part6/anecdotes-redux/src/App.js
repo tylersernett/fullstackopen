3dbb2021a7ac96bb64476a644681b0ec9edbc7ac
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-18 11:01:31 -0400

    Sort anecdotes by vote count

--- part6/anecdotes-redux/src/App.js
+++ part6/anecdotes-redux/src/App.js
@@ -15,23 +15,25 @@ const App = () => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))    
+    dispatch(createAnecdote(content))
   }
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
+      {anecdotes
+        .sort((a, b) => b.votes - a.votes)
+        .map(anecdote =>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
           </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+        )}
       <h2>create new</h2>
       <form onSubmit={addAnecdote}>
         <div><input name='anecdote' /></div>
