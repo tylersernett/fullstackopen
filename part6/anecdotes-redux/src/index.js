8515bd29d666b60c684521c720d7b03ac85dc28c
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-22 16:12:03 -0400

    Extract store to new file

--- part6/anecdotes-redux/src/index.js
+++ part6/anecdotes-redux/src/index.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
+import store from './store'
 
-import anecdoteReducer from './reducers/anecdoteReducer'
-import filterReducer from './reducers/filterReducer'
-
-const store = configureStore({ reducer: { anecdotes: anecdoteReducer, filter: filterReducer } })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
---
+++ part6/anecdotes-redux/src/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit'
+import anecdoteReducer from './reducers/anecdoteReducer'
+import filterReducer from './reducers/filterReducer'
+
+const store = configureStore({ reducer: { anecdotes: anecdoteReducer, filter: filterReducer } })
+
+export default store
