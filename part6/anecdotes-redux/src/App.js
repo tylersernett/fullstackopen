d9ee81271a4015f735435204716c5bbd099b495c
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-25 00:24:26 -0400

    Fetch initial anecdotes from db on load

--- part6/anecdotes-redux/src/App.js
+++ part6/anecdotes-redux/src/App.js
@@ -3,15 +3,29 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import VisibilityFilter from './components/VisibilityFilter'
 import Notification from './components/Notification'
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then(anecdotes =>
+        dispatch(setAnecdotes(anecdotes))
+      )
+  }, [dispatch])
+
   return (
     <div>
       <h2>Anecdotes</h2>
-      <Notification/>
-      <VisibilityFilter/>
-      <AnecdoteList/>
-      <AnecdoteForm/>
+      <Notification />
+      <VisibilityFilter />
+      <AnecdoteList />
+      <AnecdoteForm />
     </div>
   )
 }
