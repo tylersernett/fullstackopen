414c841b9fef4a706fab6575b413ec4f249144ae
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-25 20:55:46 -0400

    Add notification for voting

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,7 +1,9 @@
 import { useQuery, useMutation, useQueryClient } from 'react-query'
+import { useContext } from 'react'
 import { getAnecdotes, updateAnecdote } from './requests'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import NotificationContext from './NotificationContext'
 
 const App = () => {
   const queryClient = useQueryClient()
@@ -12,9 +14,13 @@ const App = () => {
       queryClient.setQueryData('anecdotes', anecdotes.map(anecdote => anecdote.id === updatedAnecdote.id ? updatedAnecdote : anecdote))
     }
   })
+
+  const [notification, dispatch] = useContext(NotificationContext)
+
   const handleVote = (anecdote) => {
     console.log('vote')
     updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
+    dispatch({type:'SUC', payload: `voted for ${anecdote.content}`})
   }
 
   const result = useQuery('anecdotes', getAnecdotes)
