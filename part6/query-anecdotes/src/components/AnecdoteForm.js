ad4da2c4fd397055f76bfe33eb74676f8e5a5d92
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-25 21:11:55 -0400

    Add error message

--- part6/query-anecdotes/src/components/AnecdoteForm.js
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,5 +1,7 @@
 import { getAnecdotes, createAnecdote } from '../requests'
+import {useContext} from 'react'
 import { useQuery, useMutation, useQueryClient } from 'react-query'
+import NotificationContext from '../NotificationContext'
 
 const AnecdoteForm = () => {
   const queryClient = useQueryClient()
@@ -7,14 +9,19 @@ const AnecdoteForm = () => {
     onSuccess: (newAnecdote) => {
       const anecdotes = queryClient.getQueryData('anecdotes')
       queryClient.setQueryData('anecdotes', anecdotes.concat(newAnecdote))
+    },
+    onError: () => {
+      dispatch({type:'ERR', payload: `anecdote must be ≥ 5 characters`})
     }
   })
 
+  const [notification, dispatch] = useContext(NotificationContext)
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     newAnecdoteMutation.mutate({ content, votes: 0 })
+    dispatch({type:'SUC', payload: `created new anecdote: ${content}`})
   }
 
   return (
