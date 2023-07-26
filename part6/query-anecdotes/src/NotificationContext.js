728ec707c4a371d0c388f94eb20168e6f088d6fc
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-25 21:04:55 -0400

    Add timer to reset after 5 seconds

--- part6/query-anecdotes/src/NotificationContext.js
+++ part6/query-anecdotes/src/NotificationContext.js
@@ -1,10 +1,12 @@
-import { createContext, useReducer } from 'react'
+import { createContext, useReducer, useEffect } from 'react'
 
 const notificationReducer = (state, action) => {
   switch (action.type) {
     case 'ERR':
     case 'SUC':
       return action.payload
+    case 'RESET':
+      return '';
     default:
       return state
   }
@@ -14,6 +16,17 @@ const NotificationContext = createContext()
 
 export const NotificationContextProvider = (props) => {
   const [notification, notificationDispatch] = useReducer(notificationReducer, '')
+
+  // reset the notification after 5 seconds
+  useEffect(() => {
+    if (notification) {
+      const timer = setTimeout(() => {
+        notificationDispatch({ type: 'RESET' });
+      }, 5000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [notification]);
 
   return (
     <NotificationContext.Provider value={[notification, notificationDispatch]}>
