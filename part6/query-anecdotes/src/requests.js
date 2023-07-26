49138cf51b1bd7fb21bc8e56a0497368fd7bbff4
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-07-25 20:05:49 -0400

    Add create function

--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -4,3 +4,6 @@ const baseUrl = 'http://localhost:3001/anedotes'
 
 export const getAnecdotes = () =>
   axios.get(baseUrl).then(res => res.data)
+
+export const createAnecdote = newAnecdote =>
+  axios.post(baseUrl, newAnecdote).then(res => res.data)
