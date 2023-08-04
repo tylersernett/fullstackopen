822e1128a3479775826eaea0ecd6c23a064fc044
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-04 18:27:51 -0400

    Add LOGIN_MUTATION query

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -49,3 +49,11 @@ mutation updateAuthor($name: String!, $setBornTo: Int!) {
     born
   }
 }`
+
+export const LOGIN_MUTATION = gql`
+mutation Login($username: String!, $password: String!) {
+  login(username: $username, password: $password) {
+    value
+  }
+}
+`
