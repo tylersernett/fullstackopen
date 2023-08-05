7b636c5cfd785efa99a2b06128499d981b616a61
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-05 14:03:03 -0400

    Add user genre filtering

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,9 +1,34 @@
+import { useLazyQuery } from "@apollo/client"
+import { useState } from "react"
+import { SOME_BOOKS } from "../queries"
+
 const Books = (props) => {
+  const [genreFilter, setGenreFilter] = useState('all')
+  const [getSomeBooks, { loading, data }] = useLazyQuery(SOME_BOOKS) // Use useLazyQuery
+
   if (!props.show) {
     return null
   }
 
   const books = props.books
+  const getUniqueGenres = () => {
+    const genresSet = new Set()
+    books.forEach((book) => {
+      book.genres.forEach((genre) => genresSet.add(genre))
+    })
+    return Array.from(genresSet) //return Array here so Array.map can be used below
+  }
+
+  const uniqueGenres = getUniqueGenres()
+
+  const filteredBooks = data
+    ? data.allBooks.filter((book) => genreFilter === 'all' || book.genres.includes(genreFilter))
+    : books
+
+  const handleClick = async (genre) => {
+    getSomeBooks({ variables: { genres: genre } })
+    setGenreFilter(genre)
+  }
 
   return (
     <div>
@@ -16,15 +41,26 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((book) => (
-            <tr key={book.title}>
-              <td>{book.title}</td>
-              <td>{book.author.name}</td>
-              <td>{book.published}</td>
-            </tr>
-          ))}
+          {loading ? <div>loading...</div> :
+            filteredBooks.map((book) => (
+              <tr key={book.title}>
+                <td>{book.title}</td>
+                <td>{book.author.name}</td>
+                <td>{book.published}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
+
+      {uniqueGenres.map((genre) => (
+        <button
+          key={genre}
+          onClick={() => handleClick(genre)}
+          style={{ fontWeight: genre === genreFilter ? 'bold' : 'normal' }}
+        >
+          {genre}
+        </button>
+      ))}
     </div>
   )
 }
