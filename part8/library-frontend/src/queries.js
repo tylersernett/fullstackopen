61e59c7821ba7357999f637e351b3f77f5fa429f
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-06 01:57:44 -0400

    Cleanup format, add BookDetails fragment

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -1,78 +1,91 @@
 import { gql } from '@apollo/client'
 
-export const ALL_AUTHORS = gql`
-query {
-  allAuthors  {
-    name
-    born
-    bookCount
+const BOOK_DETAILS = gql`
+  fragment BookDetails on Book {
+    title
+    author {
+      name
+    }
+    published
+    genres
   }
-}
+`
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors  {
+      name
+      born
+      bookCount
+    }
+  }
 `
 
 export const ALL_BOOKS = gql`
-query {
-  allBooks {
-    title
-    author {
-      name
+  query {
+    allBooks {
+      ...BookDetails
     }
-    published
-    genres
   }
-}
+  ${BOOK_DETAILS}
 `
 
 export const SOME_BOOKS = gql`
-query getSomeBooks($author: String, $genres: String){
-  allBooks(author: $author, genres: $genres)  {
-    title
-    author {
-      name
+  query getSomeBooks($author: String, $genres: String){
+    allBooks(author: $author, genres: $genres)  {
+      ...BookDetails
     }
-    published
-    genres
   }
-}
+  ${BOOK_DETAILS}
 `
 
 export const CREATE_BOOK = gql`
-mutation createBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
-  addBook(
-    title: $title,
-    author: $author,
-    published: $published,
-    genres: $genres
-  ) {
-    title
+  mutation createBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+    addBook(
+      title: $title,
+      author: $author,
+      published: $published,
+      genres: $genres
+    ) {
+      ...BookDetails
+    }
+  }
+  ${BOOK_DETAILS}
+`
+
+export const UPDATE_AUTHOR = gql`
+  mutation updateAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(
+      name: $name,
+      setBornTo: $setBornTo
+    ) {
+      name
+      born
+    }
+}`
+
+export const LOGIN = gql`
+  mutation Login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      value
     }
   }
 `
 
-export const UPDATE_AUTHOR = gql`
-mutation updateAuthor($name: String!, $setBornTo: Int!) {
-  editAuthor(
-    name: $name,
-    setBornTo: $setBornTo
-  ) {
-    name
-    born
+export const GET_ME = gql`
+  query {
+    me {
+      username
+      favoriteGenre
+    }
   }
-}`
-
-export const LOGIN = gql`
-mutation Login($username: String!, $password: String!) {
-  login(username: $username, password: $password) {
-    value
-  }
-}
 `
 
-export const GET_ME = gql`
-query {
-  me {
-    username
-    favoriteGenre
+export const BOOK_ADDED = gql`
+  subscription {
+    bookAdded {
+      ...BookDetails
+    }
   }
-}
+  ${BOOK_DETAILS}
 `
