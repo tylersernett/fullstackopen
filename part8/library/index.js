f3f401a4e73ea697d4e065bcb0ada2648ab48e51
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-06 00:32:05 -0400

    Remove startStandAloneServer, add express

--- part8/library/index.js
+++ part8/library/index.js
@@ -1,17 +1,23 @@
 //hours spent: 1.5 + 2 + 2.5 + 2 + 1 + 2 
 
 const { ApolloServer } = require('@apollo/server')
-const { startStandaloneServer } = require('@apollo/server/standalone')
-const { GraphQLError } = require('graphql')
-const { v1: uuid } = require('uuid')
+const { expressMiddleware } = require('@apollo/server/express4')
+const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
+const express = require('express')
+const cors = require('cors')
+const http = require('http')
+
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
+
 const Author = require('./models/author')
 const Book = require('./models/book')
 const User = require('./models/user')
+
 const typeDefs = require('./schema')
 const resolvers = require('./resolvers')
 
@@ -30,33 +36,38 @@ mongoose.connect(MONGODB_URI)
     console.log('error connection to MongoDB:', error.message)
   })
 
-// const testAuthor = new Author({
-//   name: 'Testy Test',
-// })
+const start = async () => {
+  const app = express()
+  const httpServer = http.createServer(app)
 
-// testAuthor.save().then(result => {
-//   console.log('author saved!')
-//   mongoose.connection.close()
-// })
+  const server = new ApolloServer({
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  })
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
+  await server.start()
 
-startStandaloneServer(server, {
-  listen: { port: 4000 },
-  context: async ({ req, res }) => {
-    const auth = req ? req.headers.authorization : null
-    if (auth && auth.startsWith('Bearer ')) {
-      const decodedToken = jwt.verify(
-        auth.substring(7), JWT_SECRET
-      )
-      const currentUser = await User
-        .findById(decodedToken.id) 
-      return { currentUser }
-    }
-  },
-}).then(({ url }) => {
-  console.log(` Apollo server ready at ${url}`)
-})
+  app.use(
+    '/',
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        const auth = req ? req.headers.authorization : null
+        if (auth && auth.startsWith('Bearer ')) {
+          const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET)
+          const currentUser = await User.findById(decodedToken.id)
+          return { currentUser }
+        }
+      },
+    }),
+  )
+
+  const PORT = 4000
+
+  httpServer.listen(PORT, () =>
+    console.log(`Server is now running on http://localhost:${PORT}`)
+  )
+}
+
+start()
