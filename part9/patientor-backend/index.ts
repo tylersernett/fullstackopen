111ddc54ea80ad8b78e6690358dee916f7a4a3b7
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-08 01:41:03 -0400

    Change port and use CORS

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,13 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 
 app.use(express.json());
+
+// Use the cors middleware before defining your routes
+app.use(cors());
 
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
 
-const PORT = 3002;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
