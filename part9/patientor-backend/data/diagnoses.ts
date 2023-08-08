926072a0930272c57e2dd9a7b49a9e4d40de0a8a
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-08 13:31:36 -0400

    Add typing to data itself

--- part9/patientor-backend/data/diagnoses.ts
+++ part9/patientor-backend/data/diagnoses.ts
@@ -1,4 +1,6 @@
-const data = [
+import { Diagnosis } from "../src/types";
+
+const diagnoses: Diagnosis[] = [
   {
     "code": "M24.2",
     "name": "Disorder of ligament",
@@ -78,4 +80,4 @@ const data = [
   }
 ];
 
-export default data;
+export default diagnoses;
