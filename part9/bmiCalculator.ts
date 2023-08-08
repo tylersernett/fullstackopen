8853d688f6106cd577a564b86f379ae7595c43e8
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-08 00:06:20 -0400

    Fix missing return

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -4,7 +4,7 @@ const calculateBmi = (height: number, weight: number): string => {
 
   if (bmi < 16) return 'Underweight'
   else if (bmi >= 16 && bmi < 25) return 'Normal (healthy weight)'
-  else if (bmi >= 25 && bmi < 30) 'Overweight'
+  else if (bmi >= 25 && bmi < 30) return 'Overweight'
   else return 'Obese'
 }
 
