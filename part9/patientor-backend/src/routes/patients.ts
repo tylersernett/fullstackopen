8b81503dd982b21ed207e4c677aa31fd31551aea
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-08 16:07:22 -0400

    Add toNewPatient for validation

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import patientService from '../services/patientService';
+import toNewPatient from '../utils';
 
 const router = express.Router();
 
@@ -8,15 +9,27 @@ router.get('/', (_req, res) => {
 });
 
 router.post('/', (req, res) => {
-  const { name, dateOfBirth, gender, occupation, ssn } = req.body;
-  const addedPatient = patientService.addPatient({
-    name,
-    dateOfBirth,
-    gender,
-    occupation,
-    ssn
-  });
-  res.json(addedPatient);
+  try {
+    const newPatient = toNewPatient(req.body); //perform validation here
+    const addedPatient = patientService.addPatient(newPatient);
+
+    //code before using validation:
+    // const { name, dateOfBirth, gender, occupation, ssn } = req.body;
+    // const addedPatient = patientService.addPatient({
+    //   name,
+    //   dateOfBirth,
+    //   gender,
+    //   occupation,
+    //   ssn
+    // });
+    res.json(addedPatient);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 })
 
 export default router;
