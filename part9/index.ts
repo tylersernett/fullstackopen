716a53d8d54a1ab0c048c79bd2461fda9d863c81
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-08 00:23:07 -0400

    Add bmi endpoint that uses getBmi function

--- part9/index.ts
+++ part9/index.ts
@@ -1,11 +1,25 @@
 import express from 'express';
+import { getBmi } from './bmiCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
-const PORT = 3003;
+app.get('/bmi', (req, res) => {
+  const height = req.query.height as string;
+  const weight = req.query.weight as string;
+
+  const bmiResult = getBmi(height, weight);
+
+  if ('error' in bmiResult) {
+    res.status(400).json({ error: 'malformatted parameters' });
+  } else {
+    res.json(bmiResult);
+  }
+});
+
+const PORT = 3002;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
