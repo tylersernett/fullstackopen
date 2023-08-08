ecabc57acae64bcdb84ba4762fb6c8cfd0b5d2a5
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-08 14:30:23 -0400

    Refactor to avoid "as" usage

--- part9/patientor-backend/src/services/diagnosisService.ts
+++ part9/patientor-backend/src/services/diagnosisService.ts
@@ -1,9 +1,7 @@
-import diagnosisData from '../../data/diagnoses';
+import diagnoses from '../../data/diagnoses';
 import { Diagnosis } from '../types';
 
-const diagnoses: Diagnosis[] = diagnosisData as Diagnosis[];
-
-const getDiagnoses = () => {
+const getDiagnoses = (): Diagnosis[] => {
   return diagnoses;
 };
 
