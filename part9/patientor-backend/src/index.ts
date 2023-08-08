8a9e9c54eaff3bc6ef03e8c4312a59379e2dbed5
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-08 13:26:48 -0400

    Add diagnoses router

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosesRouter from './routes/diagnoses';
 const app = express();
 
 app.use(express.json());
@@ -10,6 +11,8 @@ app.use(cors());
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 const PORT = 3001;
 
