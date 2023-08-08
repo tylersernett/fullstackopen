ab7c77728cfd41a9203fdd8c3ca965401b211be4
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-08 15:16:23 -0400

    Add POST route to addPatient

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -7,8 +7,16 @@ router.get('/', (_req, res) => {
   res.send(patientService.getNonSensitivePatients());
 });
 
-router.post('/', (_req, res) => {
-  res.send('Saving a patient!');
-});
+router.post('/', (req, res) => {
+  const { name, dateOfBirth, gender, occupation, ssn } = req.body;
+  const addedPatient = patientService.addPatient({
+    name,
+    dateOfBirth,
+    gender,
+    occupation,
+    ssn
+  });
+  res.json(addedPatient);
+})
 
 export default router;
