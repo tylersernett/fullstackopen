81e9c29b9d2a00d246e293a5348b678a6232291a
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-08 13:27:31 -0400

    Add gender type and patient, diagnosis interfaces

---
+++ part9/patientor-backend/src/types.ts
@@ -0,0 +1,16 @@
+export type Gender = 'male' | 'female' | 'other'
+
+export interface Patient {
+  id: string;
+  name: string;
+  dateOfBirth: string;
+  ssn: string;
+  gender: Gender;
+  occupation: string;
+}
+
+export interface Diagnosis {
+  code: string;
+  name: string;
+  latin?: string;
+}
