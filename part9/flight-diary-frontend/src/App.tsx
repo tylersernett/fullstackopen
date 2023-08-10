d62e0095c8358b1c0057dd4b2935c337da3a353e
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-10 02:42:35 -0400

    Add new diary form

--- part9/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary-frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
-import { Entry } from './types';
+import { Entry, NewEntry } from './types';
 import DiaryEntries from './components/DiaryEntries';
 
 function App() {
   const [entries, setEntries] = useState<Entry[]>();
+  // const [newEntry, setNewEntry] = useState<NewEntry>();
+  const [notification, setNotification] = useState('');
+  const [date, setDate] = useState('');
+  const [visibility, setVisibility] = useState('');
+  const [weather, setWeather] = useState('');
+  const [comment, setComment] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,14 +24,50 @@ function App() {
     };
 
     fetchData();
-  }, [])
+  }, []);
 
   if (!entries) {
-    return null
+    return null;
+  }
+
+  const handleSubmit = async (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    // setNewEntry({ date, visibility, weather, comment })
+    const newEntry: NewEntry = { date, visibility, weather, comment };
+    try {
+      const response = await axios.post<Entry>('http://localhost:3000/api/diaries', newEntry)
+      const createdEntry: Entry = { ...newEntry, id: response.data.id }; // Assuming the response has an "id" field
+      setEntries([...entries, createdEntry]);
+      // setDate('');
+      // setVisibility('');
+      // setWeather('');
+      // setComment('');
+    } catch (error: Error | AxiosError) {
+      console.error('Error posting new entry: ', error);
+      setNotification(error)
+    }
   }
 
   return (
     <div className="App">
+      <h2>Add new entry</h2>
+      {notification && <span>Error: {notification}</span>}
+      <form onSubmit={(e) => handleSubmit(e)}>
+        date
+        <input type='text' onChange={(e) => setDate(e.target.value)}></input>
+        <br />
+        visibility
+        <input type='text' onChange={(e) => setVisibility(e.target.value)}></input>
+        <br />
+        weather
+        <input type='text' onChange={(e) => setWeather(e.target.value)}></input>
+        <br />
+        comment
+        <input type='text' onChange={(e) => setComment(e.target.value)}></input>
+        <br />
+        <button type='submit'>add</button>
+      </form>
+
       <DiaryEntries entries={entries} />
     </div>
   );
