c86c24f8112d48d3f13ba0d0ba8b500df8f9f26f
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-09 20:07:45 -0400

    Extract Part to new file

--- part9/courseinfo/src/components/Content.tsx
+++ part9/courseinfo/src/components/Content.tsx
@@ -1,43 +1,5 @@
 import { CoursePart } from '../types'
-
-const Part = ({ part }: { part: CoursePart }) => {
-  let content: JSX.Element | null = null;
-
-  const assertNever = (value: never): never => {
-    throw new Error(
-      `Unhandled discriminated union member: ${JSON.stringify(value)}`
-    );
-  };
-
-  switch (part.kind) {
-    case "basic":
-    case "background":
-      content = (
-        <>
-          <i>{part.description}</i>
-        </>
-      );
-      break;
-
-    case "group":
-      content = (
-        <>
-          project exercises: {part.groupProjectCount}
-        </>
-      );
-      break;
-
-    default:
-      return assertNever(part);
-  }
-
-  return content &&
-    <p key={part.name}>
-      <b>{part.name} {part.exerciseCount}</b>
-      <br />
-      {content}
-    </p>;
-}
+import Part from './Part'
 
 const Content = ({ courseParts }: { courseParts: CoursePart[] }) => {
   return (
