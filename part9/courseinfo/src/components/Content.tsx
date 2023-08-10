b68b3218c8543db97b0f8913f6814afd745cf068
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-09 20:06:11 -0400

    Add Part component that uses discriminated union

--- part9/courseinfo/src/components/Content.tsx
+++ part9/courseinfo/src/components/Content.tsx
@@ -1,14 +1,49 @@
-import React from 'react'
-
 import { CoursePart } from '../types'
+
+const Part = ({ part }: { part: CoursePart }) => {
+  let content: JSX.Element | null = null;
+
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    );
+  };
+
+  switch (part.kind) {
+    case "basic":
+    case "background":
+      content = (
+        <>
+          <i>{part.description}</i>
+        </>
+      );
+      break;
+
+    case "group":
+      content = (
+        <>
+          project exercises: {part.groupProjectCount}
+        </>
+      );
+      break;
+
+    default:
+      return assertNever(part);
+  }
+
+  return content &&
+    <p key={part.name}>
+      <b>{part.name} {part.exerciseCount}</b>
+      <br />
+      {content}
+    </p>;
+}
 
 const Content = ({ courseParts }: { courseParts: CoursePart[] }) => {
   return (
     <>
       {courseParts.map((part) =>
-        <p key={part.name}>
-          {part.name} {part.exerciseCount}
-        </p>
+        <Part key={part.name} part={part} />
       )}
     </>
   )
