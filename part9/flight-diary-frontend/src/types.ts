7bcb372669cae366d9a65c0daa3272976dc81785
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-10 02:42:15 -0400

    Add BaseEntry, NewEntry

--- part9/flight-diary-frontend/src/types.ts
+++ part9/flight-diary-frontend/src/types.ts
@@ -1,7 +1,14 @@
 
-export interface Entry {
-  id: number;
+interface BaseEntry {
   date: string;
   weather: string;
   visibility: string;
+}
+
+export interface Entry extends BaseEntry {
+  id: number;
+}
+
+export interface NewEntry extends BaseEntry {
+  comment: string;
 }
