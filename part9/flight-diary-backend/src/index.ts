80bff1c5d91b664223bdfc8b5676ad93f8a24049
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-10 01:52:29 -0400

    Add cors

--- part9/flight-diary-backend/src/index.ts
+++ part9/flight-diary-backend/src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 import diaryRouter from './routes/diaries';
+
 app.use(express.json());
+app.use(cors());
 
 const PORT = 3000;
 
