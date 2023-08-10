24c053c0eb9f96195996a8d79716aa6d8976f1ff
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2023-08-10 01:53:10 -0400

    Add dataFetch from backend

--- part9/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary-frontend/src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+interface Entry {
+  id: number;
+  date: string;
+  weather: string;
+  visibility: string;
+}
 
 function App() {
+  const [entries, setEntries] = useState<Entry[]>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/diaries');
+        setEntries(response.data);
+      } catch (error) {
+        console.error('Error getting diaries:', error);
+      }
+    };
+
+    fetchData();
+  }, [])
+
+  if (!entries) {
+    return null
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+      <h2>Diary Entries</h2>
+      {entries && entries.map(entry =>
+        <p key={entry.id}>
+          <b>{entry.date}</b>
+          <br/>
+          visibility: {entry.visibility}
+          <br/>
+          weather: {entry.weather}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
     </div>
   );
 }
